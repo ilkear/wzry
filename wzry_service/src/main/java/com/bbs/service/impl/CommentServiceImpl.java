469fc800b1e4f80d2b3e5acb65d1b35af46ab3d7
xiaos <584197740@qq.com>
2019-07-27 11:57:44 +0800

    后台web模块添加domain和service依赖

--- wzry_service/src/main/java/com/bbs/service/impl/CommentServiceImpl.java
+++ wzry_service/src/main/java/com/bbs/service/impl/CommentServiceImpl.java
@@ -1,14 +1,25 @@
 package com.bbs.service.Impl;
 
 import com.bbs.dao.CommentDao;
+import com.bbs.domain.Comment;
 import com.bbs.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * @author : Xiaos.Lyn
+ * @date : 2019/7/27
+ */
 @Service
 public class CommentServiceImpl implements CommentService {
+
     @Autowired
-    private CommentDao commentDao;
+    CommentDao commentDao;
+
+    @Override
+    public void addComment(Comment c) {
+        commentDao.addComment(c);
+    }
 
     @Override
     public Integer getUserSun(String userName) {
--- wzry_web/src/main/java/com/bbs/controller/CommentController.java
+++ wzry_web/src/main/java/com/bbs/controller/CommentController.java
@@ -1,24 +1,39 @@
 package com.bbs.controller;
 
-import com.bbs.dao.CommentDao;
+import com.bbs.domain.Comment;
 import com.bbs.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Date;
+
+/**
+ * @author : Xiaos.Lyn
+ * @date : 2019/7/27
+ */
 @Controller
 @RequestMapping("/comment")
 public class CommentController {
+
     @Autowired
-    private CommentService commentService;
+    CommentService service;
 
     @RequestMapping("/publish.do")
     public ModelAndView publish(String userName){
         ModelAndView mv = new ModelAndView();
-        int userSun = commentService.getUserSun(userName);
+        int userSun = service.getUserSun(userName);
         mv.setViewName("userUpg");
         mv.addObject("publish",userSun);
         return mv;
+    }
+
+    @RequestMapping("/add.do")
+    public String add(Comment c) {
+        c.setCommentTime(new Date());
+        service.addComment(c);
+
+        return "redirect:/index.jsp";
     }
 }
